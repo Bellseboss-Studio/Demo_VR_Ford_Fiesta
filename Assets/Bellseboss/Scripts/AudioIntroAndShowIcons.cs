a446c4b9516f201623c356e9085d030c8444b9cf
PeryLoth <www.luisplata@gmail.com>
2023-03-16 15:31:36 -0500

    Update art car

--- Assets/Bellseboss/Scripts/AudioIntroAndShowIcons.cs
+++ Assets/Bellseboss/Scripts/AudioIntroAndShowIcons.cs
@@ -20,7 +20,11 @@
                 _mediator.GetAudioIntro().HideTeleportsToEnvironment();
                 ServiceLocator.Instance.GetService<IDebugMediator>().LogL("Start intro");
             })
-            .Wait(() => _mediator.GetAudioIntro().HasAudioFinished(), 0.1f)
+            .Wait(() =>
+            {
+                ServiceLocator.Instance.GetService<IDebugMediator>().LogR($"audio finished {_mediator.GetAudioIntro().HasAudioFinished()}");
+                return _mediator.GetAudioIntro().HasAudioFinished();
+            }, 0.1f)
             .Add(() =>
             {
                 ServiceLocator.Instance.GetService<IDebugMediator>().LogL("Start Car Custom");
